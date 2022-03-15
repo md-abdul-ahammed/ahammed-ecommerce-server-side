51ff028735c9540f3405106927df5c99cade81d6
md-abdul-ahammed <shagarmd2082@gmail.com>
2022-03-15 06:04:31 +0600

    ahammed-ecommerce-project

---
+++ backend/server.js
@@ -0,0 +1,42 @@
+const app = require("./app");
+const cloudinary = require("cloudinary");
+const connectDatabase = require("./config/database");
+const dotenv = require("dotenv");
+const port = process.env.PORT || 5000;
+
+// Handling Uncaught Exception
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Uncaught Exception`);
+  process.exit(1);
+});
+
+// Config
+dotenv.config({ path: "backend/config/config.env" });
+
+// Connecting to database
+connectDatabase();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome To My Ecommerce Store!");
+});
+
+const server = app.listen(port, () => {
+  console.log(`Server is working on http://localhost:${port}`);
+});
+
+// Unhandled Promise Rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
