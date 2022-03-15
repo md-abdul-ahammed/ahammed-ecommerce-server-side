6063295b0b9c122146c6f9364219459e9228fca6
md-abdul-ahammed <shagarmd2082@gmail.com>
2022-03-15 07:19:43 +0600

    fix-cors

--- backend/app.js
+++ backend/app.js
@@ -4,12 +4,14 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 const errorMiddleware = require("./middleware/error");
 
 // Config
 dotenv.config({ path: "backend/config/config.env" });
 
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
